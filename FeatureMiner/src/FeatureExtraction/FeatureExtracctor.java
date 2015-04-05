4cf2b804aa0f4ce51353b1b9a6ebf82f3b0c7fff
tcllib <jason650331@Gmail.com>
2015-04-05 23:50:52 +0100

    update print method so that it can print string instead of int

--- FeatureMiner/src/FeatureExtraction/FeatureExtracctor.java
+++ FeatureMiner/src/FeatureExtraction/FeatureExtracctor.java
@@ -5,9 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 import AprioriAlgorithm.AlgoApriori;
 import AprioriAlgorithm.FrequentItemsFinder;
+import AprioriAlgorithm.Itemset;
 import AprioriAlgorithm.Itemsets;
 
 public class FeatureExtracctor {
@@ -26,7 +29,35 @@ public class FeatureExtracctor {
 		// Applying the Apriori algorithm
 		AlgoApriori apriori = new AlgoApriori();
 		Itemsets result = apriori.runAlgorithm(minsup, input, output);
-		apriori.printStats();
-		result.printItemsets(apriori.getDatabaseSize());
+		//apriori.printStats();
+		//result.printItemsets(apriori.getDatabaseSize());
+		
+		
+		System.out.println("--------------");
+		int patternCount = 0;
+		int levelCount = 0;
+		List<List<Itemset>> levels = result.getLevels();
+		for (List<Itemset> level : levels) {
+			// print how many items are contained in this level
+			System.out.println("  L" + levelCount + " ");
+			// for each itemset
+			for (Itemset itemset : level) {
+				Arrays.sort(itemset.getItems());
+				// print the itemset
+				System.out.print("  pattern " + patternCount + ":  ");
+				//itemset.print();
+				int[] items = itemset.getItems();
+				for(int i = 0; i < items.length; i++) {
+					String word = converter.lookUpDictionary(items[i]);
+					System.out.print(word + " ");
+				}
+				// print the support of this itemset
+				System.out.print("support :  "
+						+ itemset.getRelativeSupportAsString(apriori.getDatabaseSize()));
+				patternCount++;
+				System.out.println("");
+			}
+			levelCount++;
+		}
 	}
 }
