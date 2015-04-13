59e1b55b81cb3f978b16e89c618d88ef1e9b9abf
Antoine Sachet <antoine.sac@gmail.com>
2015-04-13 16:35:08 +0100

    Refactored code
    Now featureMinig.java can be used externally to obtain a summary of a
    review given a database of review. What we coded now looks like a
    library. featureMining still has a main method.

--- FeatureMiner/src/FeatureMining.java
+++ FeatureMiner/src/FeatureMining.java
@@ -1,6 +1,4 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,7 +8,6 @@ import net.sf.json.JSONObject;
 import FeatureExtraction.FeatureExtractor;
 import FeatureRating.FeatureRater;
 import FeatureRating.Opinion;
-import FeatureRating.Sentiment;
 import FeatureRating.SentimentAnalyser;
 import FeatureRating.Summary;
 import Tagger.Util;
@@ -26,57 +23,14 @@ public class FeatureMining {
 		String dataPath = "D:\\Sachou\\UCL\\FeatureMiner\\data\\small_laptop_data";
 
 		// path to product reviews (several reviews relative to the same product, in a json file)
-		String reviewPath = "D:\\Sachou\\UCL\\FeatureMiner\\data\\full_laptop_data\\B00AYA40UC.json";
+		String reviewPath = "D:\\Sachou\\UCL\\FeatureMiner\\data\\full_laptop_data\\B004EWEZKQ.json";
 
-		// extracting the possible features from our database
-		FeatureExtractor extractor = new FeatureExtractor();
-		ArrayList<String> features = extractor.extractFeatures(dataPath);
-		//print features
-		for (String feature : features) {
-			System.out.println(feature);
-		}
-
-		ArrayList<String> commonNouns = readFile("frequent_nouns", 500);
-		ArrayList<String> cleanFeatures = new ArrayList<String>();
+		// to print intermediate messages
+		Boolean verbose = true;
 		
+		Summary summary = mineFeatures(reviewPath, dataPath, verbose);
 		
-		for(String feat: features){
-			Boolean toRemove = false;
-			for(String noun: commonNouns){
-				//System.out.println(noun);
-				if(feat.equals(noun)){
-					toRemove=true;
-					System.out.println("Feature \""+feat+"\" was filtered out as too common.");
-				} else if (feat.endsWith("s") && feat.length()>1 && feat.substring(0, feat.length()-1).equals(noun)) {
-					// testing to see if it was a plural of a noun to remove
-					toRemove=true;
-					System.out.println("Feature \""+feat+"\" was filtered out as too common.");
-				}
-			}
-			if(!toRemove){
-				cleanFeatures.add(feat);
-			}
-		}
-		System.out.println((features.size()-cleanFeatures.size())+"/"+features.size()+" features were removed.");
-		// loading up the reviews, each one in a string
-		ArrayList<String> reviews = getRawReviews(reviewPath);
-
-		for(String r: reviews){
-			System.out.println(r+"\n\n");
-		}
-		// finding the opinion for each review
-		// The opinion of a review is an arraylist of (sentence, sentiment)
-		SentimentAnalyser analyser = new SentimentAnalyser();
-		ArrayList<Opinion> opinions = analyser.findOpinion(reviews);
-
-		// Finding the features rating given the opinions
-		FeatureRater rater = new FeatureRater(cleanFeatures);
-		Summary summary = rater.summarize(opinions);
-
-
-		// Printing out the summary
 		System.out.println(summary);
-
 	}
 
 	public static ArrayList<String> getRawReviews(String filePath) {
@@ -109,5 +63,65 @@ public class FeatureMining {
 			br.close();
 		}
 		return(lines);
+	}
+
+	public static Summary mineFeature(String reviewPath, String dataPath) throws IOException {
+		return(mineFeatures(reviewPath, dataPath, false));
+	}
+	public static Summary mineFeatures(String reviewPath, String dataPath, Boolean verbose) throws IOException {
+
+		// extracting the possible features from the database
+		FeatureExtractor extractor = new FeatureExtractor();
+		ArrayList<String> features = extractor.extractFeatures(dataPath);
+		//print features
+		if(verbose)
+			for (String feature : features) {
+				System.out.println(feature);
+			}
+
+		ArrayList<String> commonNouns = readFile("frequent_nouns", 500);
+		ArrayList<String> cleanFeatures = new ArrayList<String>();
+
+
+		for(String feat: features){
+			Boolean toRemove = false;
+			for(String noun: commonNouns){
+				//System.out.println(noun);
+				if(feat.equals(noun)){
+					toRemove=true;
+					if(verbose)
+						System.out.println("Feature \""+feat+"\" was filtered out as too common.");
+				} else if (feat.endsWith("s") && feat.length()>1 && feat.substring(0, feat.length()-1).equals(noun)) {
+					// testing to see if it was a plural of a noun to remove
+					toRemove=true;
+					if(verbose)
+						System.out.println("Feature \""+feat+"\" was filtered out as too common.");
+				}
+			}
+			if(!toRemove){
+				cleanFeatures.add(feat);
+			}
+		}
+		if(verbose)
+			System.out.println((features.size()-cleanFeatures.size())+"/"+features.size()+" features were removed.");
+		// loading up the reviews, each one in a string
+		ArrayList<String> reviews = getRawReviews(reviewPath);
+
+		if(verbose)
+			for(String r: reviews){
+				System.out.println(r+"\n\n");
+			}
+		// finding the opinion for each review
+		// The opinion of a review is an arraylist of (sentence, sentiment)
+		SentimentAnalyser analyser = new SentimentAnalyser();
+		ArrayList<Opinion> opinions = analyser.findOpinion(reviews);
+
+		// Finding the features rating given the opinions
+		FeatureRater rater = new FeatureRater(cleanFeatures);
+		Summary summary = rater.summarize(opinions);
+
+		return(summary);
+
+
 	}
 }
